329b839b5a1244af811aae1275eb99ca0d05272c
Chris Wu <chris@tempo.ai>
2014-05-23 12:37:24 -0700

    base interp

--- asmt6.c
+++ asmt6.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "asmt6.h"
 
 
 int evalBinop(binopC* binop) {
@@ -24,3 +27,45 @@ int evalBinop(binopC* binop) {
 
 }
 
+
+numV *interp(void *e) {
+  char *type = ((numC *) e)->type;
+
+  if (strcmp(type, "binopC") == 0) {
+    binopC *expr = (binopC *) e;
+    return evalBinop(expr);
+  }
+  else if (strcmp(type, "numC") == 0) {
+    numV *result = malloc(sizeof(numV));
+    result->type = malloc(sizeof(char) * 100);
+    strcpy(result->type, "numV");
+
+    numC *expr = (numC *) e;
+    result->numVal = expr->numVal;
+    return result;
+  }
+  else if (strcmp(type, "ifC") == 0) {
+    ifC *expr = (ifC *) e;
+    return interp(evalIfC(expr));
+  }
+  else if (strcmp(type, "boolC") == 0) {
+    numV *result = malloc(sizeof(numV));
+    result->type = malloc(sizeof(char) * 100);
+    strcpy(result->type, "numV");
+    result->numVal = ((boolC *) e)->val;
+    return result;
+  }
+
+  return NULL;
+}
+
+int main(int argc, char const* argv[])
+{
+  numC *e = malloc(sizeof(numC));
+  e->type = malloc(sizeof(char) * 100);
+  strcpy(e->type, "numC");
+  e->numVal = 3;
+
+  printf("%lf\n", interp(e)->numVal);
+  return 0;
+}
